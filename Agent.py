f833fb0b4180b3d28d818083d6d21ce340ddbb76
elumbantobing3 <elumbantobing3@gatech.edu>
2021-10-23 16:00:19 -0400

    Adds variables and their getter and setters to the Agent class

--- Agent.py
+++ Agent.py
@@ -1,2 +1,23 @@
 class Agent:
-    pass
+    def __init__(self, length, id, position):
+        self.length = length
+        self.id = id
+        self.positon = position
+
+    def getLength(self):
+        return self.length
+    
+    def setLength(self, value):
+        self.length = value
+        
+    def getId(self):
+        return self.id
+    
+    def setId(self, value):
+        self.id = value
+        
+    def getPosition(self):
+        return self.position
+    
+    def setPosition(self, value):
+        self.position = value
