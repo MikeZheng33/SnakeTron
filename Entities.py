6cf850a49682efa3ddd5c15d2a7193518b5dc0ef
elumbantobing3 <elumbantobing3@gatech.edu>
2021-10-23 23:34:00 -0400

    Adds randomization to the apple's position after a entity collects the apple

--- Entities.py
+++ Entities.py
@@ -39,6 +39,8 @@ class Agent(Entity):
             return 'lose' + self._identifier
         if game_board[new_head[0]][new_head[1]] != '+':
             self._positions.pop(0)
+        if game_board[new_head[0]][new_head[1]] == '+':
+            return 'apple'
         if game_board[new_head[0]][new_head[1]] in ['1', '2'] and new_head != self._positions[0]:
             return 'lose'
 
--- Game.py
+++ Game.py
@@ -28,13 +28,14 @@ def main():
     entities: list[Entity] = []
 
     apple_pos = random_position()
+    apple_invalid = False
     game_board[apple_pos[0]][apple_pos[1]] = '+'
 
     player_pos = random_position()
     entities.append(Player('1', [player_pos]))
     game_board[player_pos[0]][player_pos[1]] = '1'
     frame_count: int = 0
-
+    
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -49,8 +50,9 @@ def main():
                     return
 
         game_board = [[' ' for x in range(BOARD_HEIGHT)] for y in range(BOARD_WIDTH)]
-        # if apple_invalid:
-        #     pass
+        if apple_invalid:
+            apple_pos = random_apple(game_board)
+            apple_invalid = False
         game_board[apple_pos[0]][apple_pos[1]] = '+'
         for entity in entities:
             for position in entity.get_positions():
@@ -61,6 +63,15 @@ def main():
         pygame.display.flip()
         clock.tick(5)
 
+def random_apple(game_board, range_x=None, range_y=None) -> tuple[int, int]:
+    if range_x is None:
+        range_x = range(0, BOARD_WIDTH)
+    if range_y is None:
+        range_y = range(0, BOARD_HEIGHT)
+        
+    empty_positons = [(x, y) for x in range_x for y in range_y if game_board[x][y]==' ']
+    
+    return random.choice(empty_positons)
 
 def random_position(range_x=None, range_y=None) -> tuple[int, int]:
     if range_x is None:
